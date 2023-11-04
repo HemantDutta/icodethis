f875be4bae8f96ca50f6220b65751fee1c9401f1
HemantDutta <hemantnorddutta2@gmail.com>
2023-11-04 15:27:31 +0530

    Start Nov 4

---
+++ 2023/November/nov_4/index.js
@@ -0,0 +1,32 @@
+//Menu Item Activator
+let menu_items = document.querySelectorAll(".list-item");
+menu_items.forEach((x) => {
+    x.addEventListener("click", () => {
+        x.classList.toggle("active");
+    })
+})
+
+//Close others
+function closeOthers(x) {
+    menu_items.forEach((y) => {
+        if (x !== y) y.classList.remove("active");
+    })
+
+    const item_lists = ["reminders", "todo", "groceries", "others"];
+    let curr_list = document.getElementById(item_lists[x.id]);
+    curr_list.classList.add("active");
+    item_lists.forEach((m) => {
+        if(m !== item_lists[x.id]) {
+            let curr = document.getElementById(m);
+            curr.classList.remove("active");
+        }
+    })
+}
+
+//Item checker
+let list_items = document.querySelectorAll(".items span");
+list_items.forEach((x) => {
+    x.addEventListener("click", () => {
+        x.classList.toggle("checked");
+    })
+})
